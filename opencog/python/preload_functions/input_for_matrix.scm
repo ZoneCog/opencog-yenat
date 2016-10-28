b115ddd510c7bf1e980adf3700031c9e24bfb561
yenat <yenatshif@gmail.com>
2016-10-28 09:32:10 +0300

    remove preload

--- opencog/python/preload_functions/input_for_matrix.scm
+++
@@ -1,8 +0,0 @@
-(use-modules (opencog) (opencog exec))
-(cog-execute!
-(ExecutionOutputLink 
-(GroundedSchemaNode "py: matrix_multiplication.string_to_array")
-(ListLink
-(ArrayNode "(1,2),(3,4),(5,6)")
-(ArrayNode "(1,2,3),(4,5,6)")
-)))
